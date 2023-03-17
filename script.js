874e603edf46058309803902e94e1daea07fe623
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-03-17 20:46:20 +0100

    Create calculateAge function to calculate the inputed age

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let firstInput = document.getElementById('first-input');
 let secondInput = document.getElementById('second-input');
 const btn = document.querySelector('button');
 const ageResult = document.querySelector('p');
+
+function calculateAge() {
+  let currentYear = firstInput.value;
+  let birthYear = secondInput.value;
+  let age = currentYear - birthYear;
+  ageResult.textContent = `You are ${age} Years Old!`;
+  return age;
+}
