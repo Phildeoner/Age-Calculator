77f1bbaac4d4c946a4539f11b9523f04b144caf5
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-03-17 20:44:40 +0100

    Declare DOM variables in script.js file

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstInput = document.getElementById('first-input');
+let secondInput = document.getElementById('second-input');
+const btn = document.querySelector('button');
+const ageResult = document.querySelector('p');
